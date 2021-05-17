865cf0da2ff7593152e9fc69ab1afbcd09b4d292
Austin Bergman <austin.bergman7@gmail.com>
2021-05-16 22:50:37 -0700

    fixing domain issues

--- Client/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ Client/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,77 +13,77 @@ namespace SwervesBar.Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 1 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 2 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 3 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 4 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 5 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 6 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 7 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 8 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 9 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using SwervesBar.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 10 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using SwervesBar.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\Pages\FetchData.razor"
+#line 2 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\Pages\FetchData.razor"
 using SwervesBar.Shared;
 
 #line default
@@ -98,7 +98,7 @@ using SwervesBar.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 38 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\Pages\FetchData.razor"
+#line 38 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
--- Client/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ Client/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,70 +13,70 @@ namespace SwervesBar.Client.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 1 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 2 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 3 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 4 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 5 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 6 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 7 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 8 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 9 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using SwervesBar.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\_Imports.razor"
+#line 10 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\_Imports.razor"
 using SwervesBar.Client.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using SwervesBar.Client.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\Client\Shared\SurveyPrompt.razor"
+#line 12 "c:\Users\austi\OneDrive\Desktop\Swerves_Bar\SwervesBarPodcast.github.io\Client\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
