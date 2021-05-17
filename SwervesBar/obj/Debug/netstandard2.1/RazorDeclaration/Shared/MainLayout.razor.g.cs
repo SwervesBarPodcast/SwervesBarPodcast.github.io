58f388157b963b4db3077192bace6f198cc2f3f4
Austin Bergman <austin.bergman7@gmail.com>
2021-05-16 22:08:31 -0700

    fixing base on index

--- SwervesBar/obj/Debug/netstandard2.1/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ SwervesBar/obj/Debug/netstandard2.1/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,63 +13,63 @@ namespace SwervesBar.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
+#line 1 "c:\Users\austi\OneDrive\Desktop\SwervesBar\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
+#line 2 "c:\Users\austi\OneDrive\Desktop\SwervesBar\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
+#line 3 "c:\Users\austi\OneDrive\Desktop\SwervesBar\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
+#line 4 "c:\Users\austi\OneDrive\Desktop\SwervesBar\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
+#line 5 "c:\Users\austi\OneDrive\Desktop\SwervesBar\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
+#line 6 "c:\Users\austi\OneDrive\Desktop\SwervesBar\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
+#line 7 "c:\Users\austi\OneDrive\Desktop\SwervesBar\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
+#line 8 "c:\Users\austi\OneDrive\Desktop\SwervesBar\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
 using SwervesBar;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "c:\Users\austi\OneDrive\Desktop\Podcast_Site\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
+#line 9 "c:\Users\austi\OneDrive\Desktop\SwervesBar\SwervesBarPodcast.github.io\SwervesBar\_Imports.razor"
 using SwervesBar.Shared;
 
 #line default
